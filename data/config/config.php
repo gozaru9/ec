0dc7ce165c3ccdff53aa7cae5143970a9c6ec4c3
GOZARU9 <new.river0314@gmail.com>
2014-02-14 12:58:54 -0500

    directory reset

---
+++ data/config/config.php
@@ -0,0 +1,22 @@
+<?php
+define('ECCUBE_INSTALL', 'ON');
+define('HTTP_URL', 'http://localhost/ec/html/');
+define('HTTPS_URL', 'http://localhost/ec/html/');
+define('ROOT_URLPATH', '/ec/html/');
+define('DOMAIN_NAME', '');
+define('DB_TYPE', 'mysql');
+define('DB_USER', 'gozaru');
+define('DB_PASSWORD', 'gozaru');
+define('DB_SERVER', '127.0.0.1');
+define('DB_NAME', 'gozaru');
+define('DB_PORT', '');
+define('ADMIN_DIR', 'admin/');
+define('ADMIN_FORCE_SSL', FALSE);
+define('ADMIN_ALLOW_HOSTS', 'a:0:{}');
+define('AUTH_MAGIC', 'drostaweatraiprounearouriacostaistouphea');
+define('PASSWORD_HASH_ALGOS', 'sha256');
+define('MAIL_BACKEND', 'mail');
+define('SMTP_HOST', '');
+define('SMTP_PORT', '');
+define('SMTP_USER', '');
+define('SMTP_PASSWORD', '');
